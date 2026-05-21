b64961229001cf1ff8302634143fd1ceb48c73d5
prajithv <prajith.velicheti@gmail.com>
2026-05-21 09:53:23 -0500

    Fix nonexistent path issues and add multiprocessing

--- poc/missing_rois.py
+++ poc/missing_rois.py
@@ -5,9 +5,11 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 import sys
+import os
+from multiprocessing import Pool
 
 data_loc = "/home/012/b/be/dal144042/fmri_data"
-participants_df = pd.read_csv(f"{data_loc}/participants_200_list.csv")
+participants_df = pd.read_csv(f"{data_loc}/participants_200_list.csv")[["subid", "image", "run"]].drop_duplicates()
 mni_template = f"{data_loc}/mni/MNI152_T1_2mm_brain.nii.gz"
 v85_fmri = f"{data_loc}/perfect_example/sub-NDARINV003RTV85_ses-baselineYear1Arm1_task-sst_run-02LN.feat"
 v85_ln = f"{v85_fmri}/sub-NDARINV003RTV85_filtered_func_data_LN.nii.gz"
@@ -29,45 +31,41 @@ def get_diff_area(image):
     area = sum(xored.ravel().astype(np.float64))
     return area
 
-def verify(image):
-    return get_diff_area(image) < 80000
+def verify(area):
+    return int(area < 80000)
 
 def data_path(subid, image, run):
     return f"{data_loc}/sub-{subid}_ses-baselineYear1Arm1_task-sst_{run}LN.feat/stats_roi/sub-{subid}_{image}_{run}_LN.nii.gz"
 
+def get_result(fmri):
+    fmri_path = data_path(fmri[0], fmri[1], fmri[2])
+    if os.path.exists(fmri_path):
+        image = None
+        if len(img.load_img(fmri_path).shape) == 4:
+            image = img.load_img(img.index_img(fmri_path, 0))
+        else:
+            image = img.load_img(fmri_path)
+        area = get_diff_area(image)
+        passed = verify(area)
+        return (*fmri, area, passed)
+    return (*fmri, -1, -1)
+
 def main():
     if len(sys.argv) == 1:
-        fmris = list(participants_df[["subid", "image", "run"]].itertuples(index = False, name = None))
-        images = []
-        for fmri in fmris:
-            fmri_path = data_path(fmri[0], fmri[1], fmri[2])
-            if len(img.load_img(fmri_path).shape) == 4:
-                images.append(img.load_img(img.index_img(fmri_path, 0)))
-            else:
-                images.append(img.load_img(fmri_path))
-        areas = [get_diff_area(image) for image in images]
-        checks = [verify(image) for image in images]
-        # Reference: https://nilearn.github.io/dev/auto_examples/01_plotting/plot_transparency.html#sphx-glr-auto-examples-01-plotting-plot-transparency-py
-        # fig, axes = plt.subplots(4, 6, figsize = (15, 15))
-        # for i, _ in enumerate(images):
-        #     image = images[i]
-        #     area = areas[i]
-        #     row = int(i / 6)
-        #     col = i % 6
-        #     nplt.plot_glass_brain(image, display_mode = "x", title = f"{area}", axes = axes[row][col], colorbar = False)
-        # fig.savefig("./results/plot.png")
-        results = []
+        fmris = list(participants_df.itertuples(index = False, name = None))
+        pool = Pool()
+        results = pool.map(get_result, fmris)
         num_passed = 0
-        for fmri, area, check in zip(fmris, areas, checks):
-            results.append((*fmri, area, check))
-            num_passed += int(check)
+        total = 0
+        for result in results:
+            passed = result[-1]
+            if passed != -1:
+                num_passed += passed
+                total += 1
         with open("./scores.txt", "w", encoding = "utf-8") as file:
-            print("subid", "image", "run", "score", "passed", f"({num_passed}/{len(results)})", file = file)
+            print("subid", "image", "run", "score", "passed", f"({num_passed}/{total})", file = file)
             for result in results:
                 print(*result, file = file)
-        # good_image = img.load_img(img.index_img(v85_ln, 0))
-        # good_area = get_diff_area(good_image)
-        # nplt.plot_glass_brain(good_image, display_mode = "x", title = f"{good_area}", colorbar = False, output_file = "./results/good_plot.png")
     elif len(sys.argv) == 4:
         subid = sys.argv[1]
         image = sys.argv[2]
